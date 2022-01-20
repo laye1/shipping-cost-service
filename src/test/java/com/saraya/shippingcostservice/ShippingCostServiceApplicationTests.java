8061d5b8d0135eb77d65a957234a60fac2529ca1
laye ndiaye <layendiaye195@gmail.com>
2022-01-20 14:08:19 +0000

    first commit

---
+++ src/test/java/com/saraya/shippingcostservice/ShippingCostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saraya.shippingcostservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingCostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
